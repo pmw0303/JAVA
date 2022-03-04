78a8bf8fe644b4081d1636ae1dde713d4ca1c43f
504 <504@DESKTOP-5A5LKLO>
2022-03-04 11:38:08 +0900

    4일차

---
+++ aaa/src/Day04/Day04_3.java
@@ -0,0 +1,20 @@
+package Day04;
+
+import java.util.Scanner;
+
+public class Day04_3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int p; // 사용자 가 선택한 번호
+		int c; // 컴퓨터가 선택한 난수
+		int match; //총 게임 수
+		int pw; int cw; // 각각 승리 수
+		
+
+		
+		
+	}
+}
