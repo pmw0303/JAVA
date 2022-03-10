41f1ddbe9fd5eddead26a0191d8a8b28a0e4d206
504 <504@DESKTOP-5A5LKLO>
2022-03-10 12:04:33 +0900

    7일차

---
+++ aaa/src/Day07/Day07_3.java
@@ -0,0 +1,13 @@
+package Day07;
+
+public class Day07_3 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		// p.227 내부호출
+		Calculator2 myCalc = new Calculator2();
+		myCalc.execute(); // 외부에서 호출
+	}
+}
