25996a6d0f1911e6d82796215c5abb743617faa9
mm <mm@DESKTOP-H9LNQIB>
2022-03-14 23:04:17 +0900

    엘리베이터 시도중

---
+++ aaa/src/엘리베이터/Elevator.java
@@ -0,0 +1,15 @@
+package ø§∏Æ∫£¿Ã≈Õ;
+
+import java.util.Scanner;
+
+public class Elevator {
+
+	public static void main(String[] args) {
+		
+	
+		
+		Elevate e = new Elevate();
+		e.ask();
+		
+	}
+}
