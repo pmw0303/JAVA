87211cf39ba1d24d7fdfeb8c7de4cdc43cbd984e
504 <504@DESKTOP-F07912N>
2022-03-15 14:32:56 +0900

    10일차

---
+++ aaa/src/Day10/Fifa.java
@@ -0,0 +1,22 @@
+package Day10;
+
+public class Fifa implements GamePad{
+
+	@Override
+	public void Abtn() {
+		// TODO Auto-generated method stub
+		System.out.println("슛");
+	}
+	@Override
+	public void Bbtn() {
+		// TODO Auto-generated method stub
+		System.out.println("태클");
+	}
+	@Override
+	public void Cbtn() {
+		// TODO Auto-generated method stub
+		System.out.println("교체");
+	}
+	
+	
+}
