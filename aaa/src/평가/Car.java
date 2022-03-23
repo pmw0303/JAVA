25264b8b98c230da774f536fec9f43420101603f
504 <504@DESKTOP-5GM5QKH>
2022-03-23 13:52:28 +0900

    ss

--- aaa/src/평가/Car.java
+++ aaa/src/평가/Car.java
@@ -1,5 +1,76 @@
 package 평가;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Car {
 
+	private String carNum;
+	private String day;
+	private String carIn;
+	private String carOut;
+	private String money;
+	private LocalDateTime now;
+	
+	public Car() {}
+	
+	public Car(String carNum, LocalDateTime now) {
+		super();
+		this.carNum = carNum;
+		this.day = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		this.carIn = now.format(DateTimeFormatter.ofPattern("HH:mm"));
+		this.carOut = "주차 중";
+		this.money = "정산 전";
+		this.now = now;
+	}
+
+	public String getCarNum() {
+		return carNum;
+	}
+
+	public void setCarNum(String carNum) {
+		this.carNum = carNum;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getCarIn() {
+		return carIn;
+	}
+
+	public void setCarIn(String carIn) {
+		this.carIn = carIn;
+	}
+
+	public String getCarOut() {
+		return carOut;
+	}
+
+	public void setCarOut(String carOut) {
+		this.carOut = carOut;
+	}
+
+	public String getMoney() {
+		return money;
+	}
+
+	public void setMoney(String money) {
+		this.money = money;
+	}
+
+	public LocalDateTime getNow() {
+		return now;
+	}
+
+	public void setNow(LocalDateTime now) {
+		this.now = now;
+	}
+	
+	
 }
